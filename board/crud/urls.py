3199ce1e7eb1ae0758c85c06095b5588137d7433
jiyoungKim <rlawldud.kim99@gmail.com>
2022-01-21 20:44:06 +0900

    feat: write.html, update.html to django template

--- board/crud/urls.py
+++ board/crud/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'crud'
+# 여러개의 app을 만들때 동일한 url이름이 있는 경우 template사용시 엇갈리지 않게하기 위해 사용
 urlpatterns = [
     path('', views.index, name='index'),
+    path('write/', views.write, name='write'),
     path('<int:post_id>/', views.detail, name='detail'),
-    path('<int:post_id>/write/', views.write, name='write'),
+    path('<int:post_id>/update/', views.update, name='update'),
 ]
--- board/crud/views.py
+++ board/crud/views.py
@@ -1,6 +1,7 @@
 from multiprocessing import context
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Post
 
 def index(request):
@@ -9,10 +10,21 @@ def index(request):
     #return HttpResponse("Hello, world. You're at the crud index.")
     return render(request, 'crud/index.html', context)
 
+def write(request):
+    return render(request, 'crud/write.html')
+
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
+    try:
+        selected_post = post.choice_set.get(pk=request.POST['title'])
+    except (KeyError, Post.DoesNotExist):
+        return render(request, 'crud/write.html', {'post':post, 'error_message':"You didn't write post.",})
+    else:
+        selected_post.save()
     #return HttpResponse("You're looking at post %s." %post_id)
-    return render(request, 'crud/detail.html', {'post':post})
+    #return (request, 'crud/detail.html', {'post':post})
+    return HttpResponseRedirect(reverse('crud:index', args=(post.id,)))
 
-def write(request, post_id):
-    return HttpResponse("You're writing a post %s." %post_id)
+def update(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'crud/update.html', {'post':post})

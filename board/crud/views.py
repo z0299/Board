6ba9f8907dde539301af75ac0cf3740a517c1b9f
jiyoungKim <rlawldud.kim99@gmail.com>
2022-01-24 21:23:38 +0900

    feat: CRUD function

--- board/crud/views.py
+++ board/crud/views.py
@@ -1,5 +1,5 @@
 from multiprocessing import context
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import Post
@@ -11,20 +11,33 @@ def index(request):
     return render(request, 'crud/index.html', context)
 
 def write(request):
-    return render(request, 'crud/write.html')
+    if request.method == 'POST':
+        post = Post()
+        post.post_title = request.POST['title']
+        post.post_content = request.POST['content']
+        post.save()
+        return redirect('crud:index')
+    #return render(request, 'crud/write.html')
+    else:
+        post = Post.objects.all()
+        #return render(request, 'crud/write.html', {'post':post})
+        return render(request, 'crud/write.html',)
 
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    try:
-        selected_post = post.choice_set.get(pk=request.POST['title'])
-    except (KeyError, Post.DoesNotExist):
-        return render(request, 'crud/write.html', {'post':post, 'error_message':"You didn't write post.",})
-    else:
-        selected_post.save()
     #return HttpResponse("You're looking at post %s." %post_id)
-    #return (request, 'crud/detail.html', {'post':post})
-    return HttpResponseRedirect(reverse('crud:index', args=(post.id,)))
-
+    return render(request, 'crud/detail.html', {'post':post})
+    #return HttpResponseRedirect(reverse('crud:index', args=(post.id,)))
+    
+    
 def update(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'crud/update.html', {'post':post})
+    #posting = get_object_or_404(Post, pk=post_id)
+    #return render(request, 'crud/update.html', {'post':post})
+    if request.method == 'POST':
+        post = get_object_or_404(Post, pk=post_id)
+        post.post_content = request.POST['content']
+        post.save()
+        return redirect('crud:index')
+    else:
+        post = get_object_or_404(Post, pk=post_id)
+        return render(request, 'crud/update.html', {'post':post})
